2c4939417b867bd0583d0abec4bd95a155bae3b0
agilulfe <l.nicaud@eleves-alsacienne.org>
2022-09-21 23:55:45 +0200

    basic image viewer

--- src/helloworld/app.py
+++ src/helloworld/app.py
@@ -4,23 +4,43 @@ My first application
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+import os
 
 
 class HelloWorld(toga.App):
-
     def startup(self):
-        """
-        Construct and show the Toga application.
+        self.main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        self.path = os.path.dirname(os.path.abspath(__file__))+"/resources/edt/"
+        self.edt_list = os.listdir(self.path)
+        print("path", self.edt_list)
+        self.index = 0
+
+        self.view = toga.ImageView(id="view", image=self.path+self.edt_list[self.index])
+        self.view.style.padding_top = 50
+        self.view.style.padding_left = 50
+        self.view.style.padding_right = 50
+        self.view.style.padding_bottom = 50
+
+        # self.view_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        # self.view_box.add(self.view)
+
+        button = toga.Button(
+            'Next planning',
+            on_press=self.switch_planning,
+            style=Pack(padding=5)
+        )
+
+        self.main_box.add(button)
+        self.main_box.add(self.view)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = self.main_box
         self.main_window.show()
+
+    def switch_planning(self, widget):
+        self.index = (self.index + 1) % len(self.edt_list)
+        self.view.image = toga.Image(self.path+self.edt_list[self.index])
 
 
 def main():
---
+++ src/helloworld/scrapper.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+"""""""""""Student Info"""""""""""
+
+student_language = "Roumain"
+student_level = "L1"
+student_UE3_1 = "ECOA130d"
+student_UE3_2 = "ECOA130g"
+
+##################################
+
+DRIVER_PATH = 'chromedriver'
+
+options = Options()
+options.headless = False    # flag to decide if the page is displayed (False) or not (True)
+options.add_argument("--window-size=1920,1200")
+# options.add_argument("--window-size=970,600") #bad dimensions
+
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+driver.get("https://planning.inalco.fr/public")
+
+with open("source_code.html", "w") as file:
+    file.write(driver.page_source)
+# print(driver.page_source)
+
+delay = 3 # timeout delay in seconds
+
+#WEBPAGE
+try: # wait for the page to load
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[1].bouton_Edit')))
+    print("Page is ready!")
+except TimeoutException:
+    print("Loading of page took too much time!")
+
+#MAIN PLANNING
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(student_language + " " + student_level)
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(Keys.RETURN)
+
+try: # wait for the table to load
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[7]')))
+    print("Table is ready!")
+except TimeoutException:
+    print("Loading of table took too much time!")
+
+edt = driver.find_element(By.ID, "GInterface.Instances[1].Instances[7]")
+edt.screenshot(student_language + student_level + "_edt.png")
+print("=> Main planning saved")
+
+#SWITCH TO "MATIERES" SECTION
+driver.find_element(By.ID, "GInterface.Instances[0].Instances[1]_Combo2").click()
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[0].bouton_Edit").click()
+time.sleep(0.5)
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[0]_Liste").send_keys(Keys.ARROW_DOWN)
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[0]_Liste").send_keys(Keys.RETURN)
+
+
+#LOAD UE3 PLANNINGS
+try: # wait for the table to load
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[1].bouton_Edit')))
+    print("Matieres section is ready!")
+except TimeoutException:
+    print("Loading of matieres section took too much time!")
+
+#FIRST UE3
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(student_UE3_1)
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(Keys.RETURN)
+try: # wait for the ue3_1 to load
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[7]')))
+    print("First UE3 is ready!")
+except TimeoutException:
+    print("Loading of first UE3 took too much time!")
+edt_ue3_1 = driver.find_element(By.ID, "GInterface.Instances[1].Instances[7]")
+time.sleep(0.5)
+edt_ue3_1.screenshot(student_UE3_1 + "_edt.png")
+print("=> First UE3 planning saved")
+
+#SECOND UE3
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").clear()
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(student_UE3_2)
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(Keys.RETURN)
+try: # wait for the ue3_1 to load
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[7]')))
+    print("Second UE3 is ready!")
+except TimeoutException:
+    print("Loading of second UE3 took too much time!")
+edt_ue3_2 = driver.find_element(By.ID, "GInterface.Instances[1].Instances[7]")
+time.sleep(0.5)
+edt_ue3_2.screenshot(student_UE3_2 + "_edt.png")
+print("=> Second UE3 planning saved")
+
+
+########################
+if not options.headless:
+    time.sleep(3)
+driver.quit()
+print("Page closed.")
